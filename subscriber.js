f814d17d7fe26cad482ad5e7c340cc8d5ec8e5e0
Enrico-Busetto <enricobusetto1409@gmail.com>
2026-05-26 16:46:18 +0200

    - Aggiunte le liste "In Preparazione" e "Completati" nella pagina del subscriber - Implementata la logica del timer per spostare automaticamente gli elementi tra le liste - Aggiornata l'interfaccia utente per mostrare il cambio di stato in tempo reale

--- subscriber.js
+++ subscriber.js
@@ -1,9 +1,26 @@
 const client = mqtt.connect('ws://127.0.0.1:9001/mqtt');
 
+let lista_prep = [];
+let lista_finito = [];
+
 document.addEventListener('DOMContentLoaded', (event) => {
     client.on('connect', (event) => console.log("Subscriber connesso al Broker!"));
 
-    document.getElementById('btnSub').addEventListener('click', () => {
+
+    setInterval(() => operazioneFinita(),7000);
+    
+    function operazioneFinita(){
+        if(lista_prep.length > 0){
+            console.log("Sono entrato");
+            lista_finito.push(lista_prep.shift());
+            console.log(lista_prep);
+            console.log(lista_finito);
+            caricaListaFinito();
+            caricaListaPrep();
+        }
+    }
+
+    document.getElementById('btnSub').addEventListener('click', (event) => {
         const topic = document.getElementById('topicSub').value;
         if(topic != "") {
             client.subscribe(topic);
@@ -16,9 +33,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     client.on('message', (topic, message) => {
-        const div = document.getElementById('ricezione');
-        div.innerHTML += `<p><b>${topic}:</b> ${message.toString()}</p>`;
-        console.log(`Ricevuto su ${topic}: ${message.toString()}`);
+        const nuovoElemento = {
+            topic: topic,
+            message: message.toString()
+        };
+
+        lista_prep.push(nuovoElemento);
+        caricaListaPrep();
     });
-    
 });
+
+function caricaListaPrep(){
+    const sezione = document.getElementById("listaPrep");
+    sezione.innerHTML = "";
+    let contenutoTemporaneo = "";
+
+    for (let i = 0; i < lista_prep.length; i++) {
+        contenutoTemporaneo += `<p><b>${lista_prep[i].topic}:</b> ${lista_prep[i].message}</p>`;
+    }
+
+    sezione.innerHTML += contenutoTemporaneo;     
+}
+
+function caricaListaFinito(){
+    const sezione = document.getElementById("listaFinito");
+    sezione.innerHTML = "";
+    let contenutoTemporaneo = "";
+
+    for (let i = 0; i < lista_finito.length; i++) {
+        contenutoTemporaneo += `<p><b>${lista_finito[i].topic}:</b> ${lista_finito[i].message}</p>`;
+    }
+    sezione.innerHTML += contenutoTemporaneo;
+}
